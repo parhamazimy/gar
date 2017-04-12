f73c8b6bcca059b7a9ee88ebe8aa13525906c9f1
parham <parhamazimy@gmail.com>
2017-04-12 21:58:33 +0430

    create master page user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,18 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-class admin extends CI_Controller {
+class user extends CI_Controller
+{
     function __construct()
     {
         parent::__construct();
         $this->load->helper('url');
         $this->blade->data( 'root' , base_url() );
     }
-
-    public function index()
+    function index()
     {
-        echo "hello";
-
-
+        $this->blade->data( 'title' , 'پنل کاربری' );
+        $this->blade->display('user.masteruser');
     }
+
+
 }
